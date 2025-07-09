d7b78f55d5dcb20bff1ecf220a0260fafc7e95d4
Kewayne <34725741+Kewayne@users.noreply.github.com>
2025-07-08 23:34:53 -0500

    Create provider.php

---
+++ classes/privacy/provider.php
@@ -0,0 +1,9 @@
+<?php
+namespace smsgateway_customapi\privacy;
+use core_privacy\local\metadata\null_provider;
+class provider implements null_provider {
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
+
