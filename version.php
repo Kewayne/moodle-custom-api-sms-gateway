291c1015d6490f2ce0a810bc7a82a164dbc65ba7
kewayne <34725741+kewayne@users.noreply.github.com>
2025-07-13 14:56:38 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'smsgateway_customapi';
-$plugin->version = 2025070800;
+$plugin->version = 2025070801;
 $plugin->requires = 2024031100; // Requires Moodle 4.5+.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
